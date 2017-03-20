aeef50adbde8986b2a30ecb8fcccfcd7e7f9375e
Daniel Koics <koics.dani@gmail.com>
2017-03-20 22:14:44 +0100

    Add documentation comments to Rotation.cs

--- Rubick/Rotation.cs
+++ Rubick/Rotation.cs
@@ -2,19 +2,61 @@
 
 namespace Rubick
 {
+    /// <summary cref="Rotation">
+    /// This class is intended to store the rotation of a cell in the cube.
+    /// <para>There are 24 values for a Rotation object and an extra undefined value is also supported.</para>
+    /// <para>Instances can be created either by specifing the matrix of the rotation, or from binary form.</para>
+    /// <para>Both the matrix and the binary form can be retrieved.</para>
+    /// </summary>
     public class Rotation
     {
+        // the spirit:
         private sbyte[,] matrix;
 
+        // Properties:
+
+        /// <summary cref="Defined"> Signs if the current object is a defined Rotation with a valid Matrix. </summary>
         public bool Defined { get { return matrix != null ? true : false; } }
+
+        /// <summary cref="Matrix"> Retrieves rotation-matrix. </summary>
         public sbyte[,] Matrix { get { return matrix; } }
 
+        // Constructors:
+
+        /// <summary cref="Rotation(sbyte[,])">
+        /// Constructs Rotation object from matrix.
+        /// <para>
+        /// <list type="bullet">
+        /// <item><paramref name="matrix"/> must have size of 3x3.</item>
+        /// <item>None of the elements in <paramref name="matrix"/> can be other than 1, 0 or -1.</item>
+        /// <item><paramref name="matrix"/> must have exactly 3 non-zero elements.</item>
+        /// <item>Each non-zero element must be in a separate row and column.</item>
+        /// <item><paramref name="matrix"/> must have determinant value of 1.</item>
+        /// </list>
+        /// </para>
+        /// </summary>
+        /// <param name="matrix">
+        /// A 3x3 Array of sbyte-s as the matrix form of the Rotation.
+        /// <para>
+        /// <list type="bullet">
+        /// <item>Matrix must have size of 3x3.</item>
+        /// <item>None of the elements in matrix can be other than 1, 0 or -1.</item>
+        /// <item>Matrix must have exactly 3 non-zero elements.</item>
+        /// <item>Each non-zero element must be in a separate row and column.</item>
+        /// <item>Matrix must have determinant value of 1.</item>
+        /// </list>
+        /// </para>
+        /// </param>
         public Rotation(sbyte[,] matrix)
         {
             this.matrix = matrix;
             EnsureValidity();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="binary">A 32-bit integer representing the bit-sample of the rotation-matrix, or -1 if undefined.</param>
         public Rotation(int binary)
         {
             if (binary == -1)  // only-ones means
@@ -64,7 +106,7 @@ namespace Rubick
                     case Validator.RotationValidity.NotSixZeros:
                         throw new ArgumentOutOfRangeException("matrix", "Matrix must have exactly 3 non-zero elements");
                     case Validator.RotationValidity.Singular:
-                        throw new ArgumentOutOfRangeException("matrix", "Each non-zero elements must be in a separate row and column");
+                        throw new ArgumentOutOfRangeException("matrix", "Each non-zero element must be in a separate row and column");
                     case Validator.RotationValidity.Mirrored:
                         throw new ArgumentOutOfRangeException("matrix", "Matrix must have determinant value of 1");
                     case Validator.RotationValidity.Valid:
